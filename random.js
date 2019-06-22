3613030fedb60bbf1a04105a7557923e9df54550
sunyamare <sunyamare@gmail.com>
2019-06-22 21:26:11 +0200

    fdsa

---
+++ random.js
@@ -0,0 +1,11 @@
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet: () => {
+    console.log(this.dietType);
+  }
+};
+
+goat.diet(); // Prints undefined
