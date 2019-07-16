e92544f3ff9586afa9d4f7158e44869b4751c6c0
sunyamare <sunyamare@t-air.local>
2019-07-16 21:16:13 +0200

    fdsa

---
+++ exercises/team-stats.js
@@ -0,0 +1,40 @@
+const team = {
+    _players: [
+      {
+        firstName: 'Pablo',
+        lastName: 'Sanchez',
+        age: 21
+      },
+      {
+        firstName: 'Mark',
+        lastName: 'Zuckerberg',
+        age: 31
+      },
+      {
+        firstName: 'Franz',
+        lastName: 'Kohler',
+        age: 24
+      }, 
+      // get players() {
+
+     // }
+    ],
+    _games: [
+      {
+        opponent: 'Broncos',
+        teamPoints: 42,
+        opponentPoints: 27
+      },
+      {
+        opponent: 'Dooshs',
+        teamPoints: 4,
+        opponentPoints: 80
+      },
+      {
+        opponent: 'FCB',
+        teamPoints: 99,
+        opponentPoints: 3
+      }
+    ]
+  };
+  
