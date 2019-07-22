ef1d8fe10bded85779eb9f9f1ffbd3e605248180
sunyamare <sunyamare@gmail.com>
2019-07-22 17:41:36 +0200

    added a test for numbers

--- code-challenges/reverse-array.js
+++ code-challenges/reverse-array.js
@@ -7,13 +7,13 @@ const reverseArray = array => {
     return newArray
 }
 
-
 // When you're ready to test your code, uncomment the below and run:
 const sentence = ['sense.','make', 'all', 'will', 'This'];
-
 console.log(reverseArray(sentence)) 
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
 
-/* (turns out there's a method for that: .reverse) */ 
+// Test on numbers:
+const sequence = [1, 2, 3, 4, 5];
+console.log(reverseArray(sequence)) // [ 5, 4, 3, 2, 1 ]
 
-
+/* (turns out there's also a method for that: .reverse) */ 
