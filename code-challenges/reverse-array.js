835e8ce7d522c0bb9fa47d7466e7972e94d62417
sunyamare <sunyamare@gmail.com>
2019-07-23 10:38:24 +0200

    fixed loop to account for varying array lengths

--- code-challenges/reverse-array.js
+++ code-challenges/reverse-array.js
@@ -1,19 +1,19 @@
 // Write your code here:
 const reverseArray = array => {
     let newArray=[];
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < array.length; i++) {
     newArray.unshift(array[i]);
     };
     return newArray
 }
 
-// When you're ready to test your code, uncomment the below and run:
-const sentence = ['sense.','make', 'all', 'will', 'This'];
+// Test with a bunch of words:
 console.log(reverseArray(sentence)) 
-// Should print ['This', 'will', 'all', 'make', 'sense.'];
+// ['This', 'will', 'all', 'make', 'sense.'];
 
 // Test on numbers:
-const sequence = [1, 2, 3, 4, 5];
-console.log(reverseArray(sequence)) // [ 5, 4, 3, 2, 1 ]
+const sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 121];
+console.log(reverseArray(sequence))
+// [ 121, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
 
 /* (turns out there's also a method for that: .reverse) */ 
