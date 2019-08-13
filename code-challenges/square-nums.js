3c04a0d52733ebf18e824f98549d1003607e33e2
sunyamare <sunyamare@gmail.com>
2019-08-13 17:28:52 +0200

    solved

--- code-challenges/square-nums.js
+++ code-challenges/square-nums.js
@@ -3,6 +3,10 @@
 You can test your function when you’re ready by passing in the numbers array or by making your own array! */
 const numbers = [2, 7, 9, 171, 52, 33, 14]
 
-const toSquare = num => num * num
+const toSquare = num => num * num;
 
 // Write your code here:
+const squareNums = arr => {
+    return arr.map(toSquare);
+}
+console.log(squareNums(numbers));
