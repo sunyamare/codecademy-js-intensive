029964cea92d6af98799137a20d6fc72607ce67b
sunyamare <sunyamare@gmail.com>
2019-08-15 17:34:00 +0200

    solved

--- code-challenges/is-the-dinner-vegan.js
+++ code-challenges/is-the-dinner-vegan.js
@@ -1,20 +1,25 @@
-/* Write a function isTheDinnerVegan() that takes in an array of food objects in the format:
-
-{name: 'cabbage', source: 'plant' }
-and returns a boolean value based on whether or not every item in the array has entirely plant-based origins.
+/* Write a function isTheDinnerVegan() that takes in an array of food objects in the format: {name: 'cabbage', source: 'plant' }
+and returns a boolean value based on whether or not every item in the array has entirely plant-based origins. */
 
 const meal = [{name: 'arugula', source: 'plant'}, {name: 'tomatoes', source: 'plant'}, {name: 'lemon', source:'plant'}, {name: 'olive oil', source: 'plant'}];
 
-isTheDinnerVegan(meal); // Should return true
-You can test your function when you’re ready by passing in the dinner array or by making your own! */
+/* You can test your function when you’re ready by passing in the dinner array or by making your own! */
 // Write your code here:
+/* first solution:
+const hasPlantSource = element => {
+    return element.source === 'plant';
+}
+const isTheDinnerVegan = array => {
+    return array.every(hasPlantSource);
+}*/
+// more elegant solution:
+const isTheDinnerVegan = array => {
+    return array.every(element => element.source === 'plant');
+}
 
-
+console.log(isTheDinnerVegan(meal)); // Should return true
 
 // Feel free to comment out the code below to test your function
-/*
 const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
-
 console.log(isTheDinnerVegan(dinner))
 // Should print false
-*/
