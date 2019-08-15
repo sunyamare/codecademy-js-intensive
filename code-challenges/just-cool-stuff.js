fc7c0be8029583f6f1d80c8bcc2fd0dc6a9442b7
sunyamare <sunyamare@gmail.com>
2019-08-15 11:36:20 +0200

    solved

--- code-challenges/just-cool-stuff.js
+++ code-challenges/just-cool-stuff.js
@@ -1,15 +1,20 @@
 /* Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays. */
-// Write your code here:
-
-
-
+// Write your code here: 
+const justCoolStuff = (array1, array2) => {
+    return array1.filter(value => array2.includes(value));
+}
+// first solution which explains the above:
+/* const justCoolStuff = (array1, array2) => {
+    const isInArray2 = (value) => {
+        return array2.includes(value);
+    }
+    return array1.filter(isInArray2);
+} */
 
 // Feel free to uncomment the code below to test your function
-/*
 const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
 
 const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
 
 console.log(justCoolStuff(myStuff, coolStuff))
 // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
-*/
