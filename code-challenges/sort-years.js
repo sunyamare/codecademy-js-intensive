f68c0cc76e33871d91226439a68575349b3ba100
sunyamare <sunyamare@gmail.com>
2019-08-15 11:13:57 +0200

    solved

--- code-challenges/sort-years.js
+++ code-challenges/sort-years.js
@@ -2,13 +2,13 @@
 
 You can test your function when you’re ready by passing in the years array or by making your own array of years! */
 // Write your code here:
-
+const sortYears = array => {
+    return array.sort().reverse();
+}
 
 
 // Feel free to uncomment the below code to test your function:
-/*
 const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
 console.log(sortYears(years))
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
-*/
