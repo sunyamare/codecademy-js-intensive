8a37daa32e77461705b8d682c109efd423cfadf6
sunyamare <sunyamare@t-air.local>
2019-08-17 08:54:39 +0200

    created the object in the function

--- code-challenges/dog-factory.js
+++ code-challenges/dog-factory.js
@@ -8,6 +8,17 @@ dogFactory('Joe', 'Pug', 27)
 // Should return { name: 'Joe', breed: 'Pug', weight: 27 } */
 // Write your code here: 
 const dogFactory = (name, breed, weight) => {
+    let object = {
+        _name: name,
+        _breed: breed,
+        _weight: weight,
+        get name() {return this._name},
+        get breed() {return this._breed},
+        get weight() {return this._weight},
+        set name(name) {this._name = name},
+        set breed(breed) {this._breed = breed},
+        set weight(weight) {this._weight = weight}
+    }
     
 }
-dogFactory('Joe', 'Pug', 27) // Should return { name: 'Joe', breed: 'Pug', weight: 27 }
+console.log(dogFactory('Joe', 'Pug', 27)) // Should return { name: 'Joe', breed: 'Pug', weight: 27 }
